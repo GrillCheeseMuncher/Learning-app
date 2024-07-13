643a2d8cd299405f3727d7fc2e56702e85ed767e
TheSniezek <maciek.wisniewski16@gmail.com>
2024-07-14 01:04:10 +0200

    add simple loader and button

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+        <div className="loader">
+          <div className="loader-smol"></div>
+        </div>
+        <button className='guzior'>GUZIOR</button>
     </div>
   );
 }
