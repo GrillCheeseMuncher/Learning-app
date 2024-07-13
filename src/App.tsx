f0afdc464348850763cfff8e3e2f9ee30c97f2e4
Master of the Universe <r.michniuk@gmail.com>
2024-07-13 18:18:23 +0200

    styles refactor
    Co-authored-by: TheSniezek <TheSniezek@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import './App.scss';
-import { useState } from 'react';
-import { Burgir } from './components/unility/Burgir';
+import { Header } from './components/Header/Header';
 
 function App() {
-
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-  };
-
   return (
     <div className="App">
-      <nav className='header'>
-        <div className='logo'>Wedding app</div>
-        <Burgir isClicked={isClicked} handleClick={handleClick} />
-      </nav>
-        <div className={`menu ${isClicked ? 'show' : ''}`}>
-            <li><a href="#home">HOME</a></li>
-            <li><a href="#profile">PROFILE</a></li>
-            <li><a href="#invites">INVITES</a></li>
-            <li><a href="#settings">SETTINGS</a></li>
-            <li><a href="#logout">LOG OUT</a></li>
-        </div>
+      <Header />
     </div>
   );
 }
--- src/components/utilities/Burgir/Burgir.tsx
+++ src/components/utilities/Burgir/Burgir.tsx
@@ -1,15 +1,16 @@
+import './Burgir.scss';
+
 interface BurgirProps {
   isClicked: boolean;
   handleClick: () => void;
 }
 
 export const Burgir = ({ isClicked, handleClick }: BurgirProps) => {
-
   return (
     <div className={`burgir ${isClicked ? 'clicked' : ''}`} onClick={handleClick}>
-    <div className="krecha_up"></div>
-    <div className="krecha"></div>
-    <div className="krecha_down"></div>
-  </div>
-  )
-}
+      <div className="krecha_up"></div>
+      <div className="krecha"></div>
+      <div className="krecha_down"></div>
+    </div>
+  );
+};
