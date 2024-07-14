295af7c68169f6883c09b17f8a1b46322886c473
TheSniezek <maciek.wisniewski16@gmail.com>
2024-07-14 16:24:42 +0200

    update loader button

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { Header } from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <Header />
-        <div className="loader">
-          <div className="loader-smol"></div>
-        </div>
-        <button className='guzior'>GUZIOR</button>
+      <Header />       
     </div>
   );
 }
